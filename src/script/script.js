79f605969019e098c18eac5c9eaa03f36430da89
IonaSantana <ionasantana123@gmail.com>
2021-04-13 22:49:45 -0300

    Funcoes no javascript, proximo passo validar o IP digitado

--- src/script/script.js
+++ src/script/script.js
@@ -1,13 +1,35 @@
-let modo = document.getElementById('modo');
+
 
 function decbin()
 {
-	if (document.getElementById('modo').value == "Binário") 
+	// Escolha do usuário digitar em binário ou em decimal, aqui ele escolhe um desses modos
+	// Validar se o que o usuário digitou é válido dentro das especificações do IPV4
+	let modo = document.getElementById('modo');
+	
+	if (modo.value == "Binário") 
 	{
-		document.getElementById('modo').value = "Decimal";
+		modo.value = "Decimal";
 	}else
 	{
-		document.getElementById('modo').value = "Binário";
+		modo.value = "Binário";
 	}
 	
+}
+
+function mascara()
+{
+	let rede = document.getElementById('masc');
+	let subrede = document.getElementById('mascSub');
+
+}
+
+function calcula()
+{
+	let um = document.getElementById('1');
+	let dois = document.getElementById('2');
+	let tres = document.getElementById('3');
+	let quatro = document.getElementById('4');
+
+	
+
 }
