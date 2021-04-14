1a53b4c7560dc48bfd99542c5b9b097ad29131e1
IonaSantana <ionasantana123@gmail.com>
2021-04-13 23:32:23 -0300

    Debugando botao

--- src/script/script.js
+++ src/script/script.js
@@ -1,16 +1,18 @@
 
-
+bin = true;
 function decbin()
 {
 	// Escolha do usuário digitar em binário ou em decimal, aqui ele escolhe um desses modos
 	// Validar se o que o usuário digitou é válido dentro das especificações do IPV4
+
 	let modo = document.getElementById('modo');
-	
 	if (modo.value == "Binário") 
 	{
+		bin = false;
 		modo.value = "Decimal";
 	}else
 	{
+		bin = true;
 		modo.value = "Binário";
 	}
 	
@@ -25,11 +27,23 @@ function mascara()
 
 function calcula()
 {
+
 	let um = document.getElementById('1');
 	let dois = document.getElementById('2');
 	let tres = document.getElementById('3');
 	let quatro = document.getElementById('4');
-
-	
-
+	if (bin == true) 
+	{
+		var primeira = um.value;
+		if (primeira.length == 8) 
+		{
+			
+		}else
+		{
+			alert("O tamanho deve ser 8!");
+		}
+	}else
+	{
+		
+	}
 }
