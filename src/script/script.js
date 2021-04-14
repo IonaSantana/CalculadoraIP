333cb59b34a19be4c62db4c01776a33bf899c401
IonaSantana <ionasantana123@gmail.com>
2021-04-14 18:17:29 -0300

    validando os binarios

--- src/script/script.js
+++ src/script/script.js
@@ -1,5 +1,6 @@
 
 bin = true;
+pode = true;
 function decbin()
 {
 	// Escolha do usuário digitar em binário ou em decimal, aqui ele escolhe um desses modos
@@ -28,19 +29,43 @@ function mascara()
 function calcula()
 {
 
-	let um = document.getElementById('1');
-	let dois = document.getElementById('2');
-	let tres = document.getElementById('3');
-	let quatro = document.getElementById('4');
+	let um = document.getElementById('um');
+	let dois = document.getElementById('dois');
+	let tres = document.getElementById('tres');
+	let quatro = document.getElementById('quatro');
+	pode = true;
+	// ||
 	if (bin == true) 
 	{
-		var primeira = um.value;
-		if (primeira.length == 8) 
+		var primeira = (um.value).toString();
+		var segundo = (dois.value).toString();
+		var terceiro = (tres.value).toString();
+		var quarto = (quatro.value).toString();
+		if (primeira.length == 8 && segundo.length == 8 && terceiro.length == 8 && quarto.length == 8) 
 		{
 			
+			for (var i = 0; i < 8; i++) 
+			{
+				if((primeira[i]!='1' && primeira[i]!='0') || (segundo[i]!='1' && segundo[i]!='0') || (terceiro[i]!='1' && terceiro[i]!='0') ||(quarto[i]!='1' && quarto[i]!='0'))
+				{
+					pode = false;
+					
+				}		
+			}
+
+			if (pode == false) 
+			{
+				alert("Números 0 e 1 permitidos!");
+			}else
+			{
+				alert("Deu certo!");
+
+				
+				
+			}
 		}else
 		{
-			alert("O tamanho deve ser 8!");
+			alert("O tamanho deve ser 8 em cada um!");
 		}
 	}else
 	{
